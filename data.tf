59259d2732eb57702deba176ddfeb53a649de505
Piyush Sonigra <34072376+piyushsonigra@users.noreply.github.com>
2025-07-03 15:30:05 +0530

    Update deprecated name attribute in aws_region data resource

--- data.tf
+++ data.tf
@@ -9,5 +9,5 @@ data "aws_region" "current" {}
 locals {
   aws_partition = data.aws_partition.current.partition
   account_id    = data.aws_caller_identity.current.account_id
-  region        = data.aws_region.current.name
+  region        = data.aws_region.current.region
 }
